97d89423680c39e34efc34212026a475368b7a3b
Jerald Gan <jeraldgantc@gmail.com>
2024-10-07 18:27:28 +1100

    feat: add a button and generate wallet on the fly + store in state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import { Posts } from '@/modules/Home/Posts';
+'use client';
 
-const Homepage = async () => {
+import * as web3 from '@solana/web3.js';
+import { useEffect, useState } from 'react';
+
+const Homepage = () => {
+  const [wallet, setWallet] = useState<web3.Keypair | null>(null);
+
+  useEffect(() => {
+    const generateWallet = async () => {
+      const newWallet = web3.Keypair.generate();
+      setWallet(newWallet);
+    };
+
+    generateWallet();
+  }, []);
+
+  console.log(wallet);
+
   return (
     <section className="grid h-full place-content-center">
-      <Posts />
+      <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+        Click me
+      </button>
+      {wallet && (
+        <div className="mt-4">
+          <p>Wallet Public Key: {wallet.publicKey.toString()}</p>
+        </div>
+      )}
     </section>
   );
 };
